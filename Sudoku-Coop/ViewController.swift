7fa0bb21c3ef9a638041802cb26cdbf9491495b1
HamDanW <81836662+HamDanW@users.noreply.github.com>
2023-05-30 20:25:25 -0700

    Initial Commit

---
+++ Sudoku-Coop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sudoku-Coop
+//
+//  Created by Ryan Wong on 5/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
